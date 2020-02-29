be115cc254ee4c3d04f232cd6baa9a46eaae7bd0
Mateus <atrujillomateus@gmail.com>
2020-02-29 00:42:41 -0500

    strip_tags() on upload.php; segurity update.

--- upload.php
+++ upload.php
@@ -1,8 +1,11 @@
 <?php
 if (isset($_POST["id"])) {
 	if (!empty($_POST["id"])) {
+		$id = trim($_POST["id"]);
+		$id = strip_tags($id);
+		$id = htmlspecialchars($id);
 		mkdir("files/");
-		move_uploaded_file($_FILES['file']['tmp_name'], "files/" . $_POST['id'] . ".jpg");
+		move_uploaded_file($_FILES['file']['tmp_name'], "files/" . $id . ".jpg");
 	}
 }
 ?>
