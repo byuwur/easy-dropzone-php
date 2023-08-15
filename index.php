f57b1fca3fc6195f61bb6f8ad9602abd0ed6f6d4
Mateus <atrujillomateus@gmail.com>
2023-08-15 14:32:55 -0500

    Updated to dropzone.js 6.0.0, better coding, the unlicensed

--- index.php
+++ index.php
@@ -1,32 +1,32 @@
 <?php
-if (isset($_GET["id"])) {
-	if (!empty($_GET["id"])) {
-		$id = trim($_GET["id"]);		// Delete whitespaces
-		$id = strip_tags($id);			// Delete HTML and PHP tags
-		$id = htmlspecialchars($id);	// Declares HTML tags
+if (!isset($_GET["id"])) die("Please send an ID through a GET request.");
+if (empty($_GET["id"])) die("Please input a valid ID.");
+$id = trim($_GET["id"]);
+$id = strip_tags($id);
+$id = htmlspecialchars($id);
 ?>
-<div class="row">
-	<link rel="stylesheet" href="dropzone/dropzone.min.css" />
-	<p style="color:#f44">*The <b>image</b> must have a maximum size of <b>-X- megabytes</b>.</p>
-	<form action="upload.php" method="POST" class="dropzone" id="dropzoneID" name="dropzoneID">
-		<input type="hidden" id="id" name="id" value="<?= $id ?>" />
-	</form>
-	<p>The <b>image</b> will be automatically uploaded once selected.</p>
-	<script src="dropzone/dropzone.min.js"></script>
-	<!-- SETTINGS -->
-	<script>
-	Dropzone.options.dropzoneID = {
-		maxFileSize: 5,	/* None max size if not declared */
-		acceptedFiles: "image/jpeg",	/* All files admitted if not declared */
-		init: function init() { this.on("error", function() {
-			alert("An ERROR occurred uploading the file.");
-		}); }
-	}
-	</script>
-	<!-- END SETTINGS -->
-	<!-- GO TO https://www.dropzonejs.com/ TO LEARN MORE -->
-</div>
-<?php
-	} else echo "Set a valid ID.";
-} else echo "Set an ID";
-?>
+<link rel="stylesheet" href="dropzone/dropzone.min.css" />
+<script src="dropzone/dropzone.min.js"></script>
+<h1>SINGLE FILE DROPZONE</h1>
+<p>ID (also serves as filename) = <b><?= $id; ?></b></p>
+<p>The <b>files</b> will be uploaded to a folder named "files/" next to this index.php file. Go check it out!</p>
+<p style="color:crimson;">*The <b>image</b> must have a maximum size of <b>five (1) megabytes</b>.</p>
+<form id="myDZ" class="dropzone" action="upload.php">
+	<input type="hidden" name="filename" value="<?= $id; ?>" />
+</form>
+<p>The <b>image</b> will be automatically uploaded once selected.</p>
+<p>This was the implementation object, use it wisely:<br><code>let myDZ = new Dropzone("#myDZ", { acceptedFiles: "image/jpeg", maxFileSize: 1, parallelUploads: 1, paramName: "myFile", resizeWidth: 100, resizeHeight: 100, resizeMimeType: "image/jpeg", resizeQuality: 0.75, resizeMethod: "contain" });</code></p>
+<script>
+	Dropzone.autoDiscover = false;
+	let myDZ = new Dropzone("#myDZ", {
+		acceptedFiles: "image/jpeg",
+		maxFileSize: 1,
+		parallelUploads: 1,
+		paramName: "myFile",
+		resizeWidth: 256,
+		resizeHeight: 256,
+		resizeMimeType: "image/jpeg",
+		resizeQuality: 0.5,
+		resizeMethod: "contain"
+	});
+</script>
--- upload.php
+++ upload.php
@@ -1,11 +1,11 @@
 <?php
-if (isset($_POST["id"])) {
-	if (!empty($_POST["id"])) {
-		$id = trim($_POST["id"]);
-		$id = strip_tags($id);
-		$id = htmlspecialchars($id);
-		mkdir("files/");
-		move_uploaded_file($_FILES['file']['tmp_name'], "files/" . $id . ".jpg");
-	}
-}
-?>
+if (!isset($_POST["filename"])) exit;
+if (empty($_POST["filename"])) exit;
+if (empty($_FILES)) exit;
+$id = trim($_POST["filename"]);
+$id = strip_tags($id);
+$id = htmlspecialchars($id);
+$filename_arr = explode(".", $_FILES['myFile']['name']);
+if (!is_dir("files/")) mkdir("files/");
+move_uploaded_file($_FILES['myFile']['tmp_name'], "files/" . $id . "." . end($filename_arr));
+exit;
